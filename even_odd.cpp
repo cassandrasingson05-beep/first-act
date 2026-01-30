ec683195737f2fbfa55b7b859d077ceed2d940d3
cassandrasingson05-beep <cassandrasingson05@gmail.com>
2026-01-30 11:42:22 +0800

    Update: added loop to allow multiple number checks

--- even_odd.cpp
+++ even_odd.cpp
@@ -3,15 +3,23 @@ using namespace std;
 
 int main() {
     int number;
+    char choice;
 
-    cout << "Enter an integer: ";
-    cin >> number;
+    do {
+        cout << "Enter an integer: ";
+        cin >> number;
 
-    if (number % 2 == 0) {
-        cout << "The number is Even." << endl;
-    } else {
-        cout << "The number is Odd." << endl;
-    }
+        if (number % 2 == 0) {
+            cout << "The number is Even." << endl;
+        } else {
+            cout << "The number is Odd." << endl;
+        }
 
+        cout << "Do you want to check another number? (y/n): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    cout << "Program ended. Thank you!" << endl;
     return 0;
 }
